e26a9d3bb05b2502465b8f52fba6760486ed8c6d
Tanya <tanakutova74@gmail.com>
2026-02-09 20:25:53 +0200

    add home page with basic statistic and base templates

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task, Worker
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    num_tasks = Task.objects.count()
+    num_workers = Worker.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "tasks/index.html", context=context)
