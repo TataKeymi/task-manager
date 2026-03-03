e8f72564c8e4bab25eb50d148373c092cb459d55
Tanya <tanakutova74@gmail.com>
2026-03-03 21:25:36 +0200

    Move SECRET_KEY to environment variable

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from decouple import config
 import os
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kytyzmw885qr86*7#+ft78b$%t4dxao9u9x5(qw$w5k@%^mgw+"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
