ef10dbc51087f27237bab351c7c250796e9c6d73
Tanya <tanakutova74@gmail.com>
2026-03-08 12:55:12 +0200

    prepare project for deployment

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from task_manager import settings
+from django.conf import settings
 
 
 class Position(models.Model):
